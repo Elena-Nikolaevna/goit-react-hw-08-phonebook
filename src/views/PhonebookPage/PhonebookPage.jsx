95240469bd6730b4257da341114451e611581af9
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-20 01:44:41 +0200

    Update PhonebookPage.jsx

--- src/views/PhonebookPage/PhonebookPage.jsx
+++ src/views/PhonebookPage/PhonebookPage.jsx
@@ -1,13 +1,28 @@
-import {ContactItem} from '../../components/ContactItem/ContactItem';
-import {ContactForm} from '../../components/ContactForm/ContactForm';
-import Filter from '../../components/Filter/Filter';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactItem/ContactItem';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/contacts/operations';
+import { Helmet } from 'react-helmet';
 
-export default function PhonebookPage() {
+export default function PhoneBook() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
-    <section>
+    <>
+      <Helmet>
+        <title>My Contacts</title>
+      </Helmet>
+
+      <h1>Phonebook</h1>
       <ContactForm />
+      <h2>Contacts</h2>
       <Filter />
-      <ContactItem />
-    </section>
+      <ContactList />
+    </>
   );
 }
