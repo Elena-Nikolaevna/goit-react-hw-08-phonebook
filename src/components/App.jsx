1c4f871770a918ad597347f53ef0d9cce7b7ecd9
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-20 01:43:59 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,52 @@
-import css from './App.module.css';
-import {AppBar} from './AppBar/AppBar';
-import { Suspense, useEffect, lazy } from 'react';
+
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { refreshUser } from '../redux/auth/operations';
-import { selectIsRefreshing } from '../redux/auth/selectors';
+import { Layout } from './Layout';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { refresh } from 'redux/auth/operations';
 
 
-const PhonebookPage = lazy(() =>
-  import('../views/PhonebookPage/PhonebookPage'),
-);
-const HomePage = lazy(() =>
-  import('../views/HomePage/HomePage'),
-);
-const SignInPage = lazy(() =>
-  import('../views/SignInPage/SignInPage'),
-);
+const HomePage = lazy(() => import('../views/HomePage/HomePage'));
+const RegisterPage = lazy(() => import('../views/SignInPage/SignInPage'));
+const LoginPage = lazy(() => import('../views/Login/Login'));
+const PhoneBook = lazy(() => import('../views/PhonebookPage/PhonebookPage'));
 
-const SignUpPage = lazy(() =>
-  import('../views/SignUpPage/SignUpPage'),
-);
-
-const App = () => {
+export const App = () => {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsRefreshing);
 
   useEffect(() => {
-    dispatch(refreshUser());
+    dispatch(refresh());
   }, [dispatch]);
 
   return (
-    !isLoading && (
-      <div>
-        <AppBar className={css.header} />
-        <main className={(css.main)}>
-          <Suspense fallback={<h1>Loading...</h1>}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />;
-              <Route path="/phonebook" element={<PhonebookPage />} />;
-              <Route path="/signIn" element={<SignInPage />} />;
-              <Route path="/signUp" element={<SignUpPage />} />;
-            </Routes>
-          </Suspense>
-        </main>
-        
-      </div>
-    )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<PhoneBook />} />
+          }
+        />
+        <Route path="*" element={<p></p>} />
+      </Route>
+    </Routes>
   );
 };
-export default App;
-
