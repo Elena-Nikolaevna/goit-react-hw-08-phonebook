34dd42f8cc0bbcbe20927e916bfe0ff365736f5f
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-20 01:44:07 +0200

    Update ContactItem.js

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -2,52 +2,37 @@
 //import PropTypes from 'prop-types';
 import css from './ContactItem.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-//import { deleteItems } from 'redux/contactsSlice';
-import {
-  deleteContacts,
-  fetchContacts,
-  getVisibleContacts,
-} from '../../redux/phonebook/selectors';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { selectIsLoggedIn } from '../../redux/auth/selectors'
+import { deleteContact } from 'redux/contacts/operations';
+import { getVisibleContacts } from 'redux/contacts/selectors';
+import Button from '@mui/material/Button';
+//import DeleteIcon from '@mui/icons-material/Delete';
 
-export const ContactItem = () => {
+export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(getVisibleContacts);
-  const navigate = useNavigate();
-  const loggedIn = useSelector(selectIsLoggedIn);
-  
-  const onDeleteContactCard = id => {
-    dispatch(deleteContacts(id));
+
+  const removeContact = contactId => {
+    dispatch(deleteContact(contactId));
   };
+
+  const visibleContacts = useSelector(getVisibleContacts);
+
   
-  useEffect(() => {
-    if (!loggedIn) {
-      return navigate('/signIn');
-    }
-  }, [loggedIn, navigate]);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   return (
     <ul className={css.contactsList}>
-      {contacts.map(({ id, name, phone }) => (
+      {visibleContacts.map(({ id, name, number }) => { return (
         <li className={css.contactsItem} key={id}>
           <span className={css.contactText}>
-            {name} : {phone}
+            {name} : {number}
           </span>
-          <button
-            type="button"
-            onClick={() => onDeleteContactCard(id)}
-            className={css.button}
-          >
+          <Button
+              variant="outlined"
+              onClick={() => removeContact(id)}
+              
+            >
             Delete
-          </button>
+          </Button>
         </li>
-      ))}
+      )})}
     </ul>
   );
 };
