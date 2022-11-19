37bf401212e19b0db78aaa80f834c96325c3da48
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-20 01:44:38 +0200

    Update SignInPage.jsx

--- src/views/SignInPage/SignInPage.jsx
+++ src/views/SignInPage/SignInPage.jsx
@@ -1,82 +1,14 @@
-import s from './SignInPage.module.css';
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { logIn } from '../../redux/auth/operations';
-import { selectIsLoggedIn } from '../../redux/auth/selectors';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+import { Helmet } from 'react-helmet';
 
-export default function SignInPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const loggedIn = useSelector(selectIsLoggedIn);
-
-  useEffect(() => {
-    if (loggedIn) {
-      return navigate('/phonebook');
-    }
-  }, [navigate, loggedIn]);
-
-  const handleChange = e => {
-    const { name, value } = e.target;
-
-    switch (name) {
-      case 'email':
-        setEmail(value);
-        break;
-      case 'password':
-        setPassword(value);
-        break;
-      default:
-        return;
-    }
-  };
-
-  const formSubmit = e => {
-    e.preventDefault();
-    dispatch(logIn({ email, password }));
-    setEmail('');
-    setPassword('');
-  };
-
+export default function Register() {
   return (
-    <section>
-      <form className={s.phonebookForm} onSubmit={formSubmit}>
-        <div className={s.inputBox}>
-          <label className={s.label}>
-            Email<span className={s.required}>*</span>
-          </label>
-          <input
-            className={s.input}
-            value={email}
-            onChange={handleChange}
-            type="email"
-            name="email"
-            required
-          />
-        </div>
-        <div className={s.inputBox}>
-          <label className={s.label}>
-            Password<span className={s.required}>*</span>
-          </label>
-          <input
-            className={s.input}
-            value={password}
-            onChange={handleChange}
-            type="password"
-            name="password"
-            title="minimum number of characters - seven"
-            required
-          />
-        </div>
-        <div className={s.buttonBox}>
-          <button className={s.addButton} type="submit">
-          Register
-          </button>
-        </div>
-      </form>
-    </section>
+    <div>
+      <Helmet>
+        <title>Registration</title>
+      </Helmet>
+
+      <RegisterForm />
+    </div>
   );
 }
