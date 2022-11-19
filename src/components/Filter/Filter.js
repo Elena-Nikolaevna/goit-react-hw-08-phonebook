1019b46e6fb213131d5238482dc1daaf0956b63f
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-19 19:10:20 +0200

    Create Filter.js

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,30 @@
+//import React from 'react';
+///import PropTypes from 'prop-types';
+import css from './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+//import { getFilter } from 'redux/contactsSlice';
+import { getFilter } from '../../redux/phonebook/selectors';
+import { filterChangeAction } from '../../redux/phonebook/actions';
+
+export default function Filter() {
+  const value = useSelector(getFilter);
+  const dispatch = useDispatch();
+  const handleFilterChange = event =>
+    dispatch(filterChangeAction(event.target.value));
+  return (
+    <div className={css.filterWrap}>
+      <label className={css.label}>Find contact by name</label>
+      <input
+        className={css.input}
+        value={value}
+        type="text"
+        onChange={handleFilterChange}
+      ></input>
+    </div>
+  );
+};
+
+/* Filter.propTypes = {
+  filter: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+}; */
