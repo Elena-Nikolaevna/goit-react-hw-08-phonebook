acc07e072f0efc8d35d0ca9d4ae9be5e88183b97
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-19 22:44:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,52 @@
-import s from './App.module.css';
-//import React, { Component } from 'react';
-import {ContactForm} from './ContactForm/ContactForm';
-import Filter from './Filter/Filter';
-import {ContactItem} from './ContactItem/ContactItem';
-//import ContactItem from './ContactItem/ContactItem';
-//import {useState,useEffect} from "react";
-//import { nanoid } from 'nanoid';
+import css from './App.module.css';
+import {AppBar} from './AppBar/AppBar';
+import { Suspense, useEffect, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { refreshUser } from '../redux/auth/operations';
+import { selectIsRefreshing } from '../redux/auth/selectors';
 
-export function App () {
+
+const PhonebookPage = lazy(() =>
+  import('../views/PhonebookPage/PhonebookPage'),
+);
+const HomePage = lazy(() =>
+  import('../views/HomePage/HomePage'),
+);
+const SignInPage = lazy(() =>
+  import('../views/SignInPage/SignInPage'),
+);
+
+const SignUpPage = lazy(() =>
+  import('../views/SignUpPage/SignUpPage'),
+);
+
+const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
-    <>
-      <div className={s.container}>
-        <h1 className={s.title}>Phonebook</h1>
-        <ContactForm />
-        <h2 className={s.title}>Contacts</h2>
-        <Filter />
-        <ContactItem />         
+    !isLoading && (
+      <div>
+        <AppBar className={css.header} />
+        <main className={(css.main)}>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />;
+              <Route path="/phonebook" element={<PhonebookPage />} />;
+              <Route path="/signIn" element={<SignInPage />} />;
+              <Route path="/signUp" element={<SignUpPage />} />;
+            </Routes>
+          </Suspense>
+        </main>
+        
       </div>
-    </>
+    )
   );
-}
-
+};
+export default App;
 
