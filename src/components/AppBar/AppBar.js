ad13466bc2bef8646bb2ca47403929082935351e
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-21 01:56:01 +0200

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,6 +1,6 @@
 import { Navigation } from '../Navigation/Navigation';
-import UserMenu from 'components/UserMenu';
-import AuthNav from 'components/AuthNav';
+import UserMenu from 'components/UserMenu/UserMenu';
+import AuthNav from 'components/AuthNav/AuthNav';
 import { useAuth } from 'hooks';
 import css from './AppBar.module.css';
 
