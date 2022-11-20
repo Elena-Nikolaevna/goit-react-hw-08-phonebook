3c31c3313f39c39de339de060d078d3572967e9e
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-21 01:56:03 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import AppBar from 'components/AppBar';
+import AppBar from 'components/AppBar/AppBar';
 import { Suspense } from 'react';
 
 const Layout = () => {
