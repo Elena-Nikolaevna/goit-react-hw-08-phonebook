79808641266bdeb46ed0d4d1dc2b8db17102b766
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-20 02:49:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,24 @@
+import s from './App.module.css';
+//import React, { Component } from 'react';
+import {ContactForm} from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import {ContactItem} from './ContactItem/ContactItem';
+//import ContactItem from './ContactItem/ContactItem';
+//import {useState,useEffect} from "react";
+//import { nanoid } from 'nanoid';
 
-import { useEffect, lazy } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
-import { PrivateRoute } from './PrivateRoute';
-import { RestrictedRoute } from './RestrictedRoute';
-import { refresh } from 'redux/auth/operations';
-
-
-const HomePage = lazy(() => import('../views/HomePage/HomePage'));
-const RegisterPage = lazy(() => import('../views/SignInPage/SignInPage'));
-const LoginPage = lazy(() => import('../views/Login/Login'));
-const PhoneBook = lazy(() => import('../views/PhonebookPage/PhonebookPage'));
-
-export const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(refresh());
-  }, [dispatch]);
-
+export function App () {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute
-              redirectTo="/contacts"
-              component={<RegisterPage />}
-            />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
-          }
-        />
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute redirectTo="/login" component={<PhoneBook />} />
-          }
-        />
-        <Route path="*" element={<p></p>} />
-      </Route>
-    </Routes>
+    <>
+      <div className={s.container}>
+        <h1 className={s.title}>Phonebook</h1>
+        <ContactForm />
+        <h2 className={s.title}>Contacts</h2>
+        <Filter />
+        <ContactItem />         
+      </div>
+    </>
   );
-};
+}
+
+
