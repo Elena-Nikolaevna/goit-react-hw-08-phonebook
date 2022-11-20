943717c8ff394957e2c1a6bb9b27dbaf5e6ccce4
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-21 00:22:20 +0200

    Update ContactList.styled.js

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+// export const ContactsList = styled.ul`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 10px;
+// `;
+
+export const ContactsList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+export const ContactListItem = styled.li`
+  border-top: 1px solid #2f2f37;
+`;
