4441c58297a0a80485499331ddf9c49a04008b71
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-20 02:50:02 +0200

    Delete Box.jsx

--- src/components/common/Box.jsx
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-import { color, space, layout, typography, flexbox } from 'styled-system';
-
-export const Box = styled('div')(space, layout, color, typography, flexbox);
