dbfe79abc913d169d51f0667ec4ce2a17ac46767
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-21 00:10:19 +0200

    Create AuthNav.js

---
+++ src/components/AuthNav/AuthNav.js
@@ -0,0 +1,17 @@
+import css from './AuthNav.module.css';
+import { NavLink } from 'react-router-dom';
+
+const AuthNav = () => {
+  return (
+    <div>
+      <NavLink className={css.link} to="/register">
+        Register
+      </NavLink>
+      <NavLink className={css.link} to="/login">
+        Log In
+      </NavLink>
+    </div>
+  );
+};
+
+export default AuthNav;
