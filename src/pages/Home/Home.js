14c3544dcfcab86b55dd593bfe2e47c27ec04cc6
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-21 23:18:22 +0200

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,37 @@
+import { Image } from '@chakra-ui/react';
+import image from './img/phonebook.png';
+
 const styles = {
-    container: {
-      minHeight: 'calc(100vh - 50px)',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontWeight: 500,
-      fontSize: 48,
-      textAlign: 'center',
-    },
-  };
-  
-  export default function Home() {
-    return (
-      <div style={styles.container}>
-        <h1 style={styles.title}>
-          Task manager welcome page{' '}
-          <span role="img" aria-label="Greeting icon">
-            💁‍♀️
-          </span>
-        </h1>
-      </div>
-    );
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 48,
+    textAlign: 'center',
+  },
+  hello: {
+    marginBottom: 100,
+    fontWeight: 700,
+    fontSize: 80,
+  },
+  text: {    
+    fontWeight: 400,
+    fontSize: 48,
   }
-  
+};
+
+export default function Home() {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>
+        <div style={styles.hello}>Hello!</div>
+        <div style={styles.text}>I am your personal phone book! </div>
+      </h1>
+      <Image src={image} alt="img" objectFit="cover" width="450px" />
+    </div>
+  );
+}
