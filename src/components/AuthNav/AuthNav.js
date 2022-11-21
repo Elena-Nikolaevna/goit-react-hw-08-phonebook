c54b92d7cb7832c8bf1745d0e72b78880af59a94
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-22 00:54:18 +0200

    Update AuthNav.js

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -2,12 +2,15 @@ import css from './AuthNav.module.css';
 import { NavLink } from 'react-router-dom';
 
 const AuthNav = () => {
+  const activeLink = ({ isActive }) =>
+    isActive ? `${css.link} ${css.activeLink}` : css.link;
   return (
-    <div>
-      <NavLink className={css.link} to="/register">
+    <div className={css.auth__nav}>
+
+      <NavLink className={activeLink} to="/register">
         Register
       </NavLink>
-      <NavLink className={css.link} to="/login">
+      <NavLink className={activeLink} to="/login">
         Log In
       </NavLink>
     </div>
